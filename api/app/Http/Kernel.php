bcb4b912c62d29dae1de8228a4833c141b002784
ShalinPatel670 <patel.5469@buckeyemail.osu.edu>
2026-01-25 21:35:23 -0500

    f

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -16,8 +16,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\CorsMiddleware::class, // Custom CORS middleware for Railway
-        // \Illuminate\Http\Middleware\HandleCors::class, // Disabled built-in CORS
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
